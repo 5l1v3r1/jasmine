a3d4efdf1ea135c6ec87bf04f822cce6f85b25f2
icecola <jialiang.fan@shanbay.com>
2019-03-12 12:25:39 +0800

    change test mysql config

--- configs/test_config.py
+++ configs/test_config.py
@@ -2,6 +2,6 @@ from configs.base_config import Config
 
 
 class TestConfig(Config):
-    DATABASE_URL = "mysql+pool://root:newpass@localhost/jasmine_test"
+    DATABASE_URL = "mysql+pool://root:newpass@mysql/jasmine_test"
 
     DEBUG = True
